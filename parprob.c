15aa19db22cb4c401aa446e01fd6e329123540e7
HSdeH <rikdehoop2000@gmail.com>
2024-10-10 00:01:17 +0200

    setup

--- parprob.c
+++ parprob.c
@@ -1,5 +1,7 @@
 /*
- * by Rik de Hoop & Nikoloas Trigonis
+ * by Rik de Hoop & Nikolaos Trigonis
+ * A Genetic Algorithm for the Partition Problem
+ * AKA: parprob.c
  */
 
 #include <stdio.h>
@@ -10,10 +12,55 @@
 #define chromlength numBlocks
 #define popSize 10
 
+int blocks[numBlocks];
+bool generation[popSize][chromlength];
+
+int readHeights() {}
+int fillChromosome() {}
+int mutate() {}
+int crossOver() {}
+int heightOfTower(bool *chromosome, bool b) {}
+int heightDif(bool *chromosome) {}
+
 int main(int argc, char **argv) {
-  int blocks[numBlocks];
-  bool generation[popSize][chromlength];
+  srand(time(NULL));
+  readHeights();
 
-  // are you allowed to initialize with (a) heuristics?
 
+  // for(int i = 0; i < numBlocks; i++) {
+  // (void)scanf("%d ", &blocks[i]);
+  // }
+
+  // are you allowed to initialize with (a) heuristic(s)?
+
+}
+
+int heightOfTower(bool *chromosome, bool b) {
+  int height = 0;
+  if (b) {
+    for (int i = 0; i < chromlength; i++) {
+      if (chromosome[i]) {
+        height += blocks[i];
+      }
+    }
+  } else {
+    for (int i = 0; i < chromlength; i++) {
+      if (!chromosome[i]) {
+        height += blocks[i];
+      }
+    }
+  }
+  return height;
+}
+
+int heightDif(bool *chromosome) {
+  int dif = 0;
+  for (int i = 0; i < chromlength; i ++) {
+    if (chromosome[i]) {
+      dif += blocks[i];
+    } else {
+      dif -= blocks[i];
+    }
+  }
+  return abs(dif);
 }
