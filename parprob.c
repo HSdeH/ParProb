6d9db7f0fb346fbe54787a404398a0fd339d7056
HSdeH <rikdehoop2000@gmail.com>
2024-10-09 21:30:05 +0200

    parprob.c

---
+++ parprob.c
@@ -0,0 +1,19 @@
+/*
+ * by Rik de Hoop & Nikoloas Trigonis
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define numBlocks 20
+#define chromlength numBlocks
+#define popSize 10
+
+int main(int argc, char **argv) {
+  int blocks[numBlocks];
+  bool generation[popSize][chromlength];
+
+  // are you allowed to initialize with (a) heuristics?
+
+}
